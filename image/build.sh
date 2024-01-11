c249ad85fd6df96d87ac4749ea1abea9df384886
David Cooper <david@dtcooper.com>
2024-01-11 15:05:28 -0500

    Use build script

---
+++ image/build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if echo "$@" | grep -viq -- '--i-know-what-i-am-doing'; then
+    cat <<EOF
+
+WARNING: This script should ONLY run inside a clean systemd-nspawn Raspberry Pi
+OS container, inside the dtcooper/rpi-image-modifier GitHub action.
+
+If you don't know what this means, you probably don't want to run this script.
+
+If you're *SURE* you know what you're doing, run this script via the following,
+
+    \$ ${0} --i-know-what-i-am-doing
+
+EOF
+    exit 1
+fi
+
+
+FILES_DIR=/mounted-github-repo/image/files
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    git \
+    libmpv2 \
+    mpv \
+    udiskie
+
+cp -v "${FILES_DIR}/99-udisks2.rules" /etc/udev/rules.d/
+cp -v "${FILES_DIR}/udiskie.service" /etc/systemd/system/
+
+systemctl enable udiskie.service
+sed -i 's/\/boot\/firmware.*defaults/\0,uid=1000,gid=1000/' /etc/fstab
+
+curl -L https://install.python-poetry.org | POETRY_HOME=/opt/poetry python3
+cp -v "${FILES_DIR}/poetry.sh" /etc/profile.d/
+su - pi -c "git clone --branch '${GITHUB_REF_NAME}' file:///mounted-github-repo/ retro-pi-tv"
+su - pi -c "cd retro-pi-tv ; git remote set-url origin 'https://github.com/${GIHUBT_REPOSITORY}.git'"
+su - pi -c "cd retro-pi-tv ; poetry config virtualenvs.in-project true"
+su - pi -c "cd retro-pi-tv ; poetry install --without=dev"
---
+++ image/files/poetry.sh
@@ -0,0 +1,3 @@
+if [ -d /opt/poetry/bin ]; then
+    export PATH="/opt/poetry/bin:${PATH}"
+fi
