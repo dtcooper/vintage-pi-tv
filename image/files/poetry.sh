a156cf14707ac4243bf41d759a5190cc7df4b86a
David Cooper <david@dtcooper.com>
2024-01-28 19:16:06 -0500

    Only include poetry-related PATHs that exist

--- image/files/poetry.sh
+++ image/files/poetry.sh
@@ -1,3 +1,7 @@
 if [ -d /opt/poetry/bin ]; then
-    export PATH="/opt/vintage-pi-tv/.venv/bin:/opt/poetry/bin:${PATH}"
+    export PATH="/opt/poetry/bin:${PATH}"
+fi
+
+if [ -d /opt/vintage-pi-tv/.venv/bin ]; then
+    export PATH="/opt/vintage-pi-tv/.venv/bin:${PATH}"
 fi
