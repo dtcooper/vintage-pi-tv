af5a682a65b10243e2224c8794644d6a0ef74a13
David Cooper <david@dtcooper.com>
2024-02-02 18:53:27 -0500

    Don't include mount_options.conf

--- image/stage/02-storage-mods/00-run.sh
+++ image/stage/02-storage-mods/00-run.sh
@@ -3,7 +3,6 @@
 install -vm 644 files/99-udisks2.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
 install -vm 644 files/50-udiskie.rules "${ROOTFS_DIR}/etc/polkit-1/rules.d/"
 install -vm 644 files/udiskie.service "${ROOTFS_DIR}/etc/systemd/system/"
-install -vm 644 files/mount_options.conf "${ROOTFS_DIR}/etc/udisks2/mount_options.conf"
 install -vDm 755 files/resize_partitions.sh "${ROOTFS_DIR}/usr/local/lib/vintage-pi-tv-sys-mods/resize_partitions.sh"
 
 sed -i 's|init=/usr/lib/raspberrypi-sys-mods/firstboot|init=/usr/local/lib/vintage-pi-tv-sys-mods/resize_partitions.sh|' "${ROOTFS_DIR}/boot/firmware/cmdline.txt"
